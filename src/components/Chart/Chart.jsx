fadf11d72c0f63bd8232ceab9edbb8f0f3040290
unknown <shinichiizumi@protonmail.ch>
2020-05-01 17:27:22 +0530

    changes on card data reflected

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -10,7 +10,7 @@ const Chart = () => {
             setDailyData (await fetchDailyData());
         }   
         fetchAPI();
-        //console.log(dailyData);
+        
     });
 
     const LineChart =(
